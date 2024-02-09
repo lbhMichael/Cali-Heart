0e1206217af8925222de8c749b05828692a5e272
lbhMichael <mndukwe@M1612745.lan>
2024-02-09 14:51:49 +0000

    updated

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,25 +1,34 @@
-<script script="ts">
-	import Carousel from './Carousel.svelte';
+<script lang="ts">
+	import { writable } from 'svelte/store';
 
-	let techniques = ['upper body', 'lower body'];
+	// Define techniques and corresponding modal states
+	let techniques = ['Technique 1', 'Technique 2', 'Technique 3'];
+	let modalStates = techniques.map(() => writable(false));
 
-	let showModal = false;
-
-	function showModalOnClick() {
-		showModal = !showModal;
+	// Function to handle modal display
+	function showModalOnClick(index: number) {
+		modalStates[index].set(true);
+		console.log(modalStates);
 	}
 </script>
 
-<section class="border-2 min-h-[580px] px-20 pt-9">
+<div>
 	<h1 class="text-h3 duration-200 inline-block text-orange-50 cursor-pointer hover:text-orange-20">
 		Find Your Skill
 	</h1>
 
 	<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
 		{#each techniques as technique, i}
-			<div on:click={showModalOnClick} class="border-2 p-4 bg-gray-200 cursor-pointer text-center">
+			<div
+				on:click={() => showModalOnClick(i)}
+				class="border-2 p-4 bg-gray-200 cursor-pointer text-center"
+			>
 				{technique}
 			</div>
 		{/each}
+
+		{#if modalStates === true}
+			<h1>I am open</h1>
+		{/if}
 	</div>
-</section>
+</div>
